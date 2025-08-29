e987ce31771b56f6cebba540cdbb7329b2ffcb5d
jm <jm@gmail.com>
2025-08-29 18:36:17 +0400

    init

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/go-utils2/time2
 
-go 1.25.0
+go 1.24.5
--- utils2.go
+++ utils2.go
@@ -2,16 +2,10 @@ package time2
 
 import "time"
 
-const (
-	endOfDayHour = 23
-	endOfDayMin  = 59
-	endOfDaySec  = 59
-)
-
 // EndOfDay 获取今天的23:59:59 -本地时间
 func EndOfDay(now time.Time) time.Time {
 	y, m, d := now.Date()
-	return time.Date(y, m, d, endOfDayHour, endOfDayMin, endOfDaySec, int(time.Second-time.Nanosecond), now.Location())
+	return time.Date(y, m, d, 23, 59, 59, int(time.Second-time.Nanosecond), now.Location())
 }
 
 // BeginOfDay 获取今天的凌晨 -本地时间
